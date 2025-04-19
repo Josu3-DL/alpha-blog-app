00e2b83663b7bf1690208048144b9a2b73c86753
Puntitoa <josueduarte2131@gmail.com>
2025-04-18 18:16:39 -0600

    merge branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
     before_save {self.email = email.downcase}
     has_many :articles
-
+    
     validates :username, presence: true,
                          uniqueness: {case_sensitive: false},
                          length: {minimum:3, maximum:25}
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema[8.0].define(version: 2025_04_18_231913) do
   create_table "articles", force: :cascade do |t|
-    t.string "title"
+    t.string "title"-
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
